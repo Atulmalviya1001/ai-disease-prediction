ca6a0d69347c301ea27689501785eaaab36f1ff0
Atulmalviya1001 <atulmalviya0205@gmail.com>
2026-05-18 20:34:10 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+# ==========================
+# DATABASE CONNECTION
+# ==========================
+conn = sqlite3.connect("health_app.db", check_same_thread=False)
+cursor = conn.cursor()
+
+# ==========================
+# USERS TABLE
+# ==========================
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT
+)
+""")
+
+# ==========================
+# REPORTS TABLE
+# ==========================
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS reports(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    disease TEXT,
+    risk REAL,
+    result TEXT
+)
+""")
+
+conn.commit()
